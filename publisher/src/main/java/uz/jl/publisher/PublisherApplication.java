fa267716e08333cba60361b9d53d39203026ac90
Shoxruh0912 <bekpulatovshoxruh9@gmail.com>
2023-01-26 09:50:11 +0500

    multi rabbit added

--- publisher/src/main/java/uz/jl/publisher/PublisherApplication.java
+++ publisher/src/main/java/uz/jl/publisher/PublisherApplication.java
@@ -1,21 +1,23 @@
 package uz.jl.publisher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Formula;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
@@ -33,7 +35,7 @@ import java.sql.Timestamp;
 @EnableRabbit
 public class PublisherApplication {
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         SpringApplication.run(PublisherApplication.class, args);
     }
 
@@ -71,7 +73,6 @@ class Transaction {
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 class TransferCreateVO {
     private String pan;
     private BigDecimal amount;
@@ -87,13 +88,16 @@ class TransactionService {
     private final TransactionRepository repository;
     private final RabbitMQService rabbitMQService;
 
-    public Transaction create(TransferCreateVO vo) {
-        Transaction transaction = Transaction.builder()
-                .pan(vo.getPan())
-                .amount(vo.getAmount())
-                .build();
+    public Transaction create( TransferCreateVO vo ) {
+        Transaction transaction = new Transaction(null, vo.getPan(), vo.getAmount(), null);
         transaction = repository.save(transaction);
-        rabbitMQService.send(transaction);
+        try {
+            rabbitMQService.send(transaction, "prod");
+            rabbitMQService.send(transaction, "dev");
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+
         return transaction;
     }
 }
@@ -108,7 +112,7 @@ class PublisherController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Transaction transfer(@RequestBody TransferCreateVO vo) {
+    public Transaction transfer( @RequestBody TransferCreateVO vo ) {
         return service.create(vo);
     }
 }
@@ -116,35 +120,69 @@ class PublisherController {
 @Configuration
 class RabbitMQConfig {
     public static final String QUEUE = "pdp_queue";
-    public static final String EXCHANGE = "pdp_exchange";
+    public static final String EXCHANGE = "common";
     public static final String ROUTING_KEY = "pdp_routing_key";
 
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE, true);
+    //    @Bean
+//    public Queue queue() {
+//        return new Queue(QUEUE, true);
+//    }
+//
+//    @Bean
+//    public TopicExchange exchange() {
+//        return new TopicExchange(EXCHANGE, true, false);
+//    }
+//
+//
+//    @Bean
+//    public Binding binding(Queue queue, TopicExchange exchange) {
+//        return BindingBuilder
+//                .bind(queue)
+//                .to(exchange)
+//                .with(ROUTING_KEY);
+//    }
+    @Bean(name = "connection-factory-dev")
+    public ConnectionFactory connectionFactoryDev() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
     }
 
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE, true, false);
+    @Bean(name = "connection-factory-prod")
+    @Primary
+    public ConnectionFactory connectionFactoryProd() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5682);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
     }
 
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder
-                .bind(queue)
-                .to(exchange)
-                .with(ROUTING_KEY);
-    }
 
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//    @Bean
+//    public ConnectionFactoryContextWrapper contextWrapper( ConnectionFactory connectionFactory ) {
+//        return new ConnectionFactoryContextWrapper(connectionFactory);
+//    }
+
+    @Bean("rabbit-template-dev")
+    @Primary
+    public RabbitTemplate rabbitTemplateDev( @Qualifier("connection-factory-dev") ConnectionFactory connectionFactory ) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean("rabbit-template-prod")
+    public RabbitTemplate rabbitTemplateProd( @Qualifier("connection-factory-prod") ConnectionFactory connectionFactory ) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
         return rabbitTemplate;
@@ -153,14 +191,24 @@ class RabbitMQConfig {
 }
 
 @Service
-@RequiredArgsConstructor
 class RabbitMQService {
 
-    private final RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplateProd;
+    private final RabbitTemplate rabbitTemplateDev;
 
-    public void send(Object message) {
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, RabbitMQConfig.ROUTING_KEY, message);
+    RabbitMQService( @Qualifier("rabbit-template-prod") RabbitTemplate rabbitTemplateProd, @Qualifier("rabbit-template-dev") RabbitTemplate rabbitTemplateDev ) {
+        this.rabbitTemplateProd = rabbitTemplateProd;
+        this.rabbitTemplateDev = rabbitTemplateDev;
     }
 
+
+    public void send( Object message, String type ) throws JsonProcessingException {
+
+        if ( type.equals("dev") ) {
+            rabbitTemplateDev.convertAndSend(RabbitMQConfig.EXCHANGE, "xt.request", new Message(new ObjectMapper().writeValueAsBytes(message)));
+        } else if ( type.equals("prod") ) {
+            rabbitTemplateProd.convertAndSend(RabbitMQConfig.EXCHANGE, "xt.request", new Message(new ObjectMapper().writeValueAsBytes(message)));
+        }
+    }
 
 }
--- subscriber/src/main/java/uz/jl/subscriber/SubscriberApplication.java
+++ subscriber/src/main/java/uz/jl/subscriber/SubscriberApplication.java
@@ -1,20 +1,20 @@
 package uz.jl.subscriber;
 
 import lombok.*;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -25,7 +25,7 @@ import java.sql.Timestamp;
 @SpringBootApplication
 public class SubscriberApplication {
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         SpringApplication.run(SubscriberApplication.class, args);
     }
 
@@ -33,50 +33,81 @@ public class SubscriberApplication {
 
 @Configuration
 class RabbitMQConfig {
-    public static final String QUEUE = "pdp_queue";
+    public static final String QUEUE = "xt_in";
     public static final String EXCHANGE = "pdp_exchange";
     public static final String ROUTING_KEY = "pdp_routing_key";
-
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE);
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
-    }
-
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder
-                .bind(queue)
-                .to(exchange)
-                .with(ROUTING_KEY);
-    }
 
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return rabbitTemplate;
+
+    @Primary
+    @Bean("connection-factory-prod")
+    public ConnectionFactory connectionFactoryProd() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5682);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
     }
+
+    @Bean(name = "connection-factory-dev")
+    public ConnectionFactory connectionFactoryDev() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean(name = "rabbit-listener-container-factory-dev")
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactoryDev(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer,
+            @Qualifier("connection-factory-dev") ConnectionFactory connectionFactory ) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+
+        factory.setDefaultRequeueRejected(false);
+
+        return factory;
+    }
+
+    @Bean(name = "rabbit-listener-container-factory-prod")
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactoryProd(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer,
+            @Qualifier("connection-factory-prod") ConnectionFactory connectionFactory ) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+
+        factory.setDefaultRequeueRejected(false);
+
+        return factory;
+    }
+
 
 }
 
 @Component
 class TransactionListener {
 
-    @RabbitListener(queues = RabbitMQConfig.QUEUE)
-    public void logTransaction(Transaction transaction) {
-        System.out.println(transaction);
+    @RabbitListener(containerFactory = "rabbit-listener-container-factory-dev", queues = RabbitMQConfig.QUEUE)
+    public void logTransactiondev( Transaction transaction ) {
+        System.out.println("from dev" + transaction);
     }
+    @RabbitListener(containerFactory = "rabbit-listener-container-factory-prod", queues = RabbitMQConfig.QUEUE)
+    public void logTransactionprod( Transaction transaction ) {
+        System.out.println("from prod" + transaction);
+    }
+//    @RabbitListener( queues = RabbitMQConfig.QUEUE)
+//    public void logTransactionprod( Transaction transaction ) {
+//        System.out.println("from prod" + transaction);
+//    }
 }
 
 
